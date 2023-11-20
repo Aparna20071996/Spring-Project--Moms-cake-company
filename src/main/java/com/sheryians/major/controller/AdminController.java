c91323af074393ee2eaee142a9f43d462b3499ff
Aparna Kesharwani <aparnakesharwani7@gmail.com>
2023-11-20 09:56:39 +0530

    2nd commit

--- src/main/java/com/sheryians/major/controller/AdminController.java
+++ src/main/java/com/sheryians/major/controller/AdminController.java
@@ -1,19 +1,26 @@
 package com.sheryians.major.controller;
 
+import com.sheryians.major.dto.ProductDTO;
 import com.sheryians.major.model.Category;
+import com.sheryians.major.model.Product;
 import com.sheryians.major.service.CategoryService;
+import com.sheryians.major.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class AdminController {
 
     @Autowired
     CategoryService categoryService;
+    @Autowired
+    ProductService productService;
     @GetMapping("/admin")
     public String adminHome(){
         return "adminHome";
@@ -33,6 +40,40 @@ public class AdminController {
         categoryService.addCategory(category);
         return "redirect:/admin/categories";
     }
+    @GetMapping("/admin/categories/delete/{id}")
+    public String deleteCat(@PathVariable int id){
+        categoryService.removeCategoryById(id);
+        return "redirect:/admin/categories";
+    }
+    @GetMapping("/admin/categories/update/{id}")
+    public String updateCat(@PathVariable int id, Model model){
+        Optional<Category> category=categoryService.getCategoryById(id);
+        if(category.isPresent()){
+            model.addAttribute("category", category.get());
+            return "categoriesAdd";
+        }else {
+            return "404";
+        }
+    }
 
-
+    //Product Section
+    @GetMapping("/admin/products")
+    public String getProducts(Model model){
+        model.addAttribute("products", productService.getAllProducts());
+        return "products";
+    }
+    @GetMapping("/admin/products/add")
+    public String getProductsAdd(Model model){
+        model.addAttribute("productDTO", new ProductDTO());
+        model.addAttribute("categories",categoryService.getAllCategory());
+        return "productsAdd";
+    }
+    @PostMapping("/admin/products/add")
+    public String productAddPost(@ModelAttribute("productDTO") ProductDTO productDTO,
+                                 @RequestParam("productImage")MultipartFile file,
+                                 @RequestParam("imgName") String imgName) throws IOException{
+        Product product = new Product();
+        product.
+        return imgName;
+    }
 }
